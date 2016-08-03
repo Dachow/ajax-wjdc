07c235047a4cb68f28e8b53355c99e7cc7affec8
Dachow <hellowd93@163.com>
2016-08-04 01:11:37 +0800

    page is okay

--- xmlhttp.js
+++ xmlhttp.js
@@ -14,7 +14,14 @@ function loadXMLDoc(url) {
             loadDesc();
             // loadInformation();
             loadContent();
-            divideItems(1);
+
+            divideItems(3);
+            // pageBtClick();
+            selectIt();
+            targetPage(1);
+            selectItems();
+
+
         }
     };
     xmlhttp.open("GET", url, true);
@@ -193,37 +200,201 @@ function addItems(targetK) {
 // 分割页面
 function divideItems (num) {
 
-    const PAGEITEM = num;
+    PAGEITEM = num;
     // console.log(getSumItems);
-    var totalItem = getSumItems;
+    totalItem = getSumItems;
     var targetPage = Math.floor(totalItem / PAGEITEM);
     var targetPageItem = totalItem % PAGEITEM;
     // console.log(targetPage, targetPageItem);
 
     // 隐藏大于i的题目
-    for(var i=PAGEITEM; i<totalItem; i++) {
-        document.getElementsByClassName("items")[i].style.display = "none";
-        var getSection = document.getElementsByTagName("section");
-        for (var j = 1; j < getSection.length-1; j++) {
-            if (i <= getSection[j].getElementsByClassName("items").length) {
-                getSection[j+1].style.display = "none";
-            }
-        }
-    }
-    // 添加分页按钮
+    // for(var i=PAGEITEM; i<totalItem; i++) {
+    //     document.getElementsByClassName("items")[i].style.display = "none";
+    //     var getSection = document.getElementsByTagName("section");
+    //     for (var j = 1; j < getSection.length-1; j++) {
+    //         if (i <= getSection[j].getElementsByClassName("items").length) {
+    //             getSection[j+1].style.display = "none";
+    //         }
+    //     }
+    // }
+
+    // 隐藏所有题目 显示指定数目的题
+    // var getSection = document.getElementsByTagName("section");
+    // var getItems = document.getElementsByClassName("items");
+    // for (var i = 1; i < totalItem; i++) {
+    //     getItems[i].style.display = "none";
+    // }
+    // for (var j = 0; j < PAGEITEM; j++) {
+    //     getItems[j].style.display = "block";
+    //     for (var k = 1; k < getSection.length-1; k++) {
+    //       if (PAGEITEM <= getSection[k].getElementsByClassName("items").length){
+    //         getSection[k+1].style.display = "none";
+    //       }
+    //     }
+    // }
+
+
+    // 添加第一页分页按钮
     if (PAGEITEM < totalItem) {
         var createPageBt1 = document.createElement("button");
         createPageBt1.setAttribute("class", "page"+1);
+        createPageBt1.setAttribute("value", 1);
+        createPageBt1.setAttribute("onclick", "targetPage(1)");
         document.getElementById("page").appendChild(createPageBt1);
         document.getElementsByClassName("page"+1)[0].innerHTML = "1";
     }
 
-//有问题
-    for (var a = 0; (a < targetPage) && (targetPageItem !== 0); a++) {
+// 添加其余页按钮
+    for (var a = 0; (a < targetPage)&&(PAGEITEM !== totalItem); a++) {
         var createPageBt = document.createElement("button");
         createPageBt.setAttribute("class", "page"+(a+2));
+        createPageBt.setAttribute("value", (a+2));
+        createPageBt.setAttribute("onclick", "targetPage"+"("+(a+2)+")");
         document.getElementById("page").appendChild(createPageBt);
         document.getElementsByClassName("page"+(a+2))[0].innerHTML = (a+2);
     }
 
+}
+
+function targetPage(event) {
+
+    // 同步下拉框
+    var getSelect = document.getElementById("selectWrap");
+    getSelect.value = event-1;
+
+
+    // 初始化隐藏所有item
+      var getSection = document.getElementsByTagName("section");
+      var getItems = document.getElementsByClassName("items");
+      for (var i = 0; i < totalItem; i++) {
+          getItems[i].style.display = "none";
+      }
+
+      // 点击第一个按钮和其它按钮
+      if (event == 1) {
+        for (var j = 0; j < PAGEITEM; j++) {
+            getItems[j].style.display = "block";
+            for (var k = 1; k < getSection.length-1; k++) {
+              if (PAGEITEM <= getSection[k].getElementsByClassName("items").length){
+                getSection[k+1].getElementsByClassName("title")[0].style.display = "none";
+              }
+            }
+        }
+      } else {
+        var getBtLength = document.getElementById("page").getElementsByTagName("button").length;
+
+        // 如果点击最后一页
+        if (event == getBtLength) {
+          for (var a = PAGEITEM * (getBtLength-1); a < totalItem; a++) {
+            getItems[a].style.display = "block";
+          }
+        } else {
+          for (var b = PAGEITEM * (event-1); b < PAGEITEM * event; b++) {
+            getItems[b].style.display = "block";
+            for (var bI = 1; bI < getSection.length-1; bI++) {
+              if (PAGEITEM * event <= getSection[bI].getElementsByClassName("items").length){
+                getSection[bI+1].getElementsByClassName("title")[0].style.display = "none";
+              }
+            }
+          }
+        }
+      }
+    }
+
+
+function selectIt() {
+
+
+  var TotalItemsL = document.createElement("span");
+  TotalItemsL.setAttribute("class", "TotalItemsL");
+  document.getElementById('content').appendChild(TotalItemsL);
+  document.getElementsByClassName("TotalItemsL")[0].innerHTML = "共"+ totalItem +"条";
+
+
+
+    var getBtWrap = document.getElementById("page");
+
+    var noticePageL = document.createElement("span");
+    noticePageL.setAttribute("class", "noticePageL");
+    document.getElementById('content').appendChild(noticePageL);
+    document.getElementsByClassName("noticePageL")[0].innerHTML = "当前第 ";
+
+    var select = document.createElement("select");
+    select.setAttribute("id", "selectWrap");
+    select.setAttribute("onclick", "targetOption"+"("+")");
+    document.getElementById('content').appendChild(select);
+
+    var noticePageR = document.createElement("span");
+    noticePageR.setAttribute("class", "noticePageR");
+    document.getElementById('content').appendChild(noticePageR);
+    document.getElementsByClassName("noticePageR")[0].innerHTML = " 页";
+
+
+    try {
+      // var getBt = getBtWrap.getElementsByTagName("button");
+
+      for (var i = 0; i < Math.ceil(totalItem / PAGEITEM); i++) {
+        var option = document.createElement("option");
+        option.setAttribute("value", i);
+
+        document.getElementById('selectWrap').appendChild(option);
+        document.getElementById("selectWrap").getElementsByTagName("option")[i].innerHTML = i+1;
+      }
+    } catch (e) {
+      // return;
+    } finally {
+      // return targetPage(targetNum);
+    }
+}
+
+// 当前第几页点击事件
+function targetOption() {
+  var target = document.getElementById("selectWrap");
+  // console.log(typeof(xx.value));
+  // 调用对应按钮
+  targetPage((parseInt(target.value)+1));
+}
+
+
+function selectItems () {
+
+  var noticeItemsL = document.createElement("span");
+  noticeItemsL.setAttribute("class", "noticeItemsL");
+  document.getElementById('content').appendChild(noticeItemsL);
+  document.getElementsByClassName("noticeItemsL")[0].innerHTML = "每页显示 ";
+
+  var select = document.createElement("select");
+  select.setAttribute("id", "selectItemsWrap");
+  select.setAttribute("onclick", "selectItemsOption"+"("+")");
+  document.getElementById('content').appendChild(select);
+
+  var noticeItemsR = document.createElement("span");
+  noticeItemsR.setAttribute("class", "noticeItemsR");
+  document.getElementById('content').appendChild(noticeItemsR);
+  document.getElementsByClassName("noticeItemsR")[0].innerHTML = " 条";
+
+  try {
+    // var getBt = getBtWrap.getElementsByTagName("button");
+    for (var i = 0; i < totalItem; i++) {
+      var option = document.createElement("option");
+      option.setAttribute("value", i);
+
+      document.getElementById('selectItemsWrap').appendChild(option);
+      document.getElementById("selectItemsWrap").getElementsByTagName("option")[i].innerHTML = i+1;
+    }
+  } catch (e) {
+    // return;
+  } finally {
+    // 更新下拉框
+    var getTargetItems = document.getElementById("selectItemsWrap");
+    getTargetItems.value = PAGEITEM-1;
+  }
+}
+
+function selectItemsOption() {
+    var target = document.getElementById("selectItemsWrap");
+    divideItems((parseInt(target.value)+1));
+    targetPage(1);
+    // targetOption();
+    // selectIt(1);
 }
